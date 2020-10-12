bed07f6e3e18200a87025dfc4ee09c1bfb68d6f1
Jason Jimenez <jasonjimenez@MacBook-Pro.lan>
2020-10-11 20:58:54 -0500

    code for review

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
-let passwordArr = (lower+upper+characters);
+let passwordArr = "";
 var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var characters = ['~','!','@','#','$','%','^','&','*','(',')','_','+','`','[',']','','{','}','|',';',':','"',',','.','/','<','>','?'];  
-var length = ['0','1','2','3','4','5','6','7','8','9'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
 
 var generateBtn = document.querySelector("#generate");
@@ -25,26 +25,34 @@ var confirmLength = window.prompt("How many characters are needed? Enter number 
 }
 
 var confirmCharacters = window.prompt("Need special characters? (ex: %, #, &) Enter 'yes' or 'no'"); {
-  if (confirmCharacters === "no") {
+  if (confirmCharacters === "no") 
+  {
     window.alert("Using characters with numbers and letters makes your password stronger. We won't ask again, but Are You Sure?");
     window.prompt("Want special characters? Enter 'yes' or 'no'")
-    if (confirmCharacters === "yes") {window.alert("Great!");}
-  } if (confirmCharacters === "no") {window.alert("Thats OK. We'll still create a strong password");}
+  }
+  if (confirmCharacters === "yes") 
+  {
+    window.alert("Great!");
+  }
+  if (confirmCharacters === "no") 
+  { 
+    window.alert("Thats OK. We'll still create a strong password");
+  }
 }
 
 var confirmLower = window.prompt("Want Lower Case letters? Enter 'yes' or 'no'."); {
     if (confirmLower === "yes") {
       window.alert("Great! One more question.");}
      
-    else (confirmLower === "no"); { 
+    if (confirmLower === "no") { 
       window.alert("OK. One more question."); }
     }
 
 var confirmUpper = window.prompt("Use upper case letters also? Enter 'yes' or 'no'"); {
   if(confirmUpper === "yes") {
-    window.alert("Great! Get your pen and paper ready!") }
+    window.alert("Awesome! Get your pen and paper ready!"); }
   
-    else (confirmUpper === "no"); {
+    if (confirmUpper === "no") {
       window.alert("OK! Get your pen and paper ready!"); }
     }
   
@@ -56,16 +64,25 @@ console.log(confirmUpper);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password 
 function writePassword() {
+
   var password = generatePassword();
   console.log(password);
   var passwordArr = password.split(",");
   var newPassword = "";
-  for (i=0; i<passwordArr.length; i++) {
-    newPassword = newPassword + passwordArr[i];
-  }
+  //for (i=0; i<length.length; i++) {
+    //newPassword = newPassword + passwordArr[i];
+  //}
   
+for(var i = 0; i <= passwordArr; i ++ ) 
+{
+  password=password + passwordArr.chartAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+}
+
+
+
   var passwordText = document.querySelector("#password");
   passwordText.value = newPassword;
 }
@@ -73,7 +90,7 @@ function writePassword() {
 // Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
 console.log(passwordArr);
-
+document.getElementById("password").passwordArr = password;
 /*
 function makeid(length) {
   var result           = '';
